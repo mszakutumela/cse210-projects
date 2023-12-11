3a72ee72bfcfe27fb1d79b1094c5b753272635d5
mszakutumela <misszandlovu@gmail.com>
2023-12-11 15:18:36 +0200

    Develop04 is now complete with no errors :-)

---
+++ .history/prove/Develop04/ListingActivity_20231211151752.cs
@@ -0,0 +1,85 @@
+using System;
+
+public class ListingActivity : Activity
+{
+    private int _userListCount;
+    private List<string> _prompts;
+    // private List<string> _listFromUser;
+    
+    public ListingActivity()
+    {
+        SetName("Listing Activity");
+        SetDescription("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+    }
+
+    public string GetRandomPrompt()
+    {
+        _prompts = new() {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?", 
+        "Who are some of your personal heroes?",
+    };
+        Random random = new();
+        return _prompts[random.Next(0,_prompts.Count)];
+
+        // return _prompts[index];
+    }
+
+    public int GetUserListCount()
+    {
+        return _userListCount;
+    }
+
+    // public void SetUserListCount()
+    // {
+    //     _userListCount = userListCount;
+    // }
+
+    public void AddUserListCount()
+    {
+        _userListCount++;
+    }
+
+    // public string GetListFromUser()
+    // {
+    //     return _listFromUser;
+    // }
+    
+    public void RunListActivity()
+    {
+        string prompt = GetRandomPrompt();
+        Console.WriteLine();
+        Console.WriteLine("List as many responses you can to the following prompt:");
+        Console.WriteLine($"___ {prompt} ___");
+        Console.Write("You may begin in: ");
+        ShowCountDown(5);
+        Console.WriteLine();
+        ListingTimer();
+        Console.WriteLine();
+        DisplayUserListCount();
+    }    
+
+    public void ListingTimer()
+    {
+        DateTime futureTime = GetFutureTime(GetDuration());
+        DateTime currentTime = GetCurrentTime();
+        
+        while (currentTime < futureTime)
+        {
+            string promptQues = GetRandomPrompt();
+            Console.Write("> ");
+            Console.ReadLine();
+            AddUserListCount();
+            currentTime = DateTime.Now;
+        }
+    } 
+
+    public void DisplayUserListCount()
+    {
+        int userListCount = GetUserListCount();
+        Console.WriteLine($"You listed {userListCount} items. ");
+    }   
+
+}
---
+++ .history/prove/Develop04/Program_20231211151756.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine();
+
+        string userInput = " ";
+        // int breathingLog = 0;
+        // int reflectingLog = 0;
+        // int listingLog = 0;
+
+        // void DisplayLog()
+        // {
+        //     Console.WriteLine("Activity Log - Current Session");
+        //     Console.WriteLine($"Breathing Activity: {breathingLog}");
+        //     Console.WriteLine($"Reflecting Activity: {reflectingLog}");
+        //     Console.WriteLine($"Listing Activity: {listingLog}");
+        //     Console.WriteLine();
+        // }
+
+        while (true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(
+                "1. Start breathing activity\n"
+                + "2. Start reflecting activity\n"
+                + "3. Start listing activity\n"
+                + "4. Quit"
+            );
+             Console.Write("Select a choice from the menu: ");
+             
+             userInput = Console.ReadLine();
+            //  Console.WriteLine(userInput);
+            Console.WriteLine();
+
+             switch(userInput)
+             {
+                case "1":
+                    BreathingActivity breathingActivity = new();
+                    breathingActivity.RunActivity();
+                    breathingActivity.RunBreathActivity();
+                    breathingActivity.DisplayEndingMessage();
+
+                    // breathingLog++;
+                    break;
+
+                case "2":
+                    ReflectingActivity reflectingActivity = new();
+                    reflectingActivity.RunActivity();
+                    reflectingActivity.RunReflectActivity();
+                    reflectingActivity.DisplayEndingMessage();
+
+                    // reflectingLog++;
+                    break;
+
+                case "3":
+                    ListingActivity listingActivity = new();
+                    listingActivity.RunActivity();
+                    listingActivity.RunListActivity();
+                    listingActivity.DisplayEndingMessage();
+
+                    // listingLog++;
+                    break;
+                
+                case "4":
+                    Environment.Exit(0);
+                    break; 
+                default:
+                    Console.WriteLine("Please enter the number from the menu option. ");
+                    Console.WriteLine();
+                    break;
+             }
+        }
+
+    }
+}
